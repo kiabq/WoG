a75c1752e55ca9a7e188553a2ed15455c953fa86
kiabq <conway.joseph@pm.me>
2022-12-01 18:50:05 +0000

    Update About component styling and use MUI SVG icons for performance/vector smoothing stuffz

--- frontend/src/Components/Pages/About.tsx
+++ frontend/src/Components/Pages/About.tsx
@@ -1,15 +1,34 @@
-// Libraries
-import React from "react";
+// Components
+import AboutCard from "./AboutCard";
 
 // Styles
 import st from "./About.module.css";
+import PublicRoundedIcon from '@mui/icons-material/PublicRounded';
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import QuestionMarkRoundedIcon from '@mui/icons-material/QuestionMarkRounded';
 
 const About = () => {
     return (
         <div className={st.about}>
-            <div>
-                <h2 className="rb_bld_h4">About</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi voluptatem, laborum fugit, aspernatur, dolorem ducimus harum nobis hic tenetur ipsa dignissimos! Ratione quibusdam amet dolor aperiam, veniam voluptate iste nam.</p>
+            <div className={st.about__card__container}>
+                <AboutCard icon={<PublicRoundedIcon sx={{ fontSize: 65, color: "#ffffff" }}/>}>
+                        <h3 className="ub_reg_h3">What is the World of Gaian?</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam et harum quasi rem quas eius accusamus error impedit nihil minima placeat, similique cumque aspernatur totam?</p>
+                </AboutCard>
+                <AboutCard icon={<PlayArrowRoundedIcon sx={{ fontSize: 80, color: "#ffffff" }}/>}>
+                        <h3 className="ub_reg_h3">How do I start?</h3>
+                        <ul className={st.about__card__list}>
+                            <li>Login with your Discord account</li>
+                            <li>Customize your account information</li>
+                        </ul>
+                </AboutCard>
+                <AboutCard icon={<QuestionMarkRoundedIcon sx={{ fontSize: 65, color: "#ffffff" }}/>}>
+                        <h3 className="ub_reg_h3">What next?</h3>
+                        <ul className={st.about__card__list}>
+                            <li>Join the Discord server</li>
+                            <li>Wait for a DM to contact you</li>
+                        </ul>
+                </AboutCard>
             </div>
         </div>
     )
