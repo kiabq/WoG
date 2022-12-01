0d8aca0e419e775608a0c2dcee486302c791c127
kiabq <conway.joseph@pm.me>
2022-12-01 18:55:59 +0000

    Update home page background

--- frontend/src/Components/Pages/HomePage.tsx
+++ frontend/src/Components/Pages/HomePage.tsx
@@ -1,6 +1,5 @@
 // Libraries
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 // Components
 import About from "./About";
@@ -8,40 +7,42 @@ import Scheduler from "../Scheduler/Scheduler";
 
 // Styles
 import st from "./Home.module.css";
-import gb from "../../globals.module.css"
 
 // Hooks
 import { useAuth } from "../../hooks/useProvider";
-import { useAcct } from "../../hooks/useAccount";
 
 const HomePage = () => {
     const auth = useAuth();
-    const navigate = useNavigate();
 
     return (
         <main className={st.main__container__content}>
             <div className={`${st.main__container__bg}`}>
-                <h1 className="rb_bld_h1">The World of Gaian</h1>
-                <div className={st.main__container__buttons}>
-                    <a href="#schedule" className={`${st.main__container__button} ${st.main__container__button__schedule}`}>
-                        <button>
-                            Game Times
-                        </button>
-                    </a>
-                    {!auth?.token && 
-                        <a href={`${process.env.REACT_APP_BACKEND_URL}/api/connect/discord`}>
-                            <button className={`${st.main__container__button} ${st.main__container__button__signin}`}>
-                                Sign in
+                <div className={`${st.main__container__cover}`}>
+                    <h1 className={`${st.main__container__title} rb_bld_h1 pd6-top`}>
+                        The World of Gaian
+                    </h1>
+                    <div className={`${st.main__container__buttons} rb_bld_h3`}>
+                        <a href="#schedule" className={`${st.main__container__button} ${st.main__container__button__schedule}`}>
+                            <button>
+                                SCHEDULE
                             </button>
                         </a>
-                    }
-                    {auth?.token && 
-                        <Link to="/lore" className={`${st.main__container__button} ${st.main__container__button__schedule}`}>
-                            <button>
-                                Lore
-                            </button>
-                        </Link>
-                    }
+                        {!auth?.token && 
+                            <a href={`${process.env.REACT_APP_BACKEND_URL}/api/connect/discord`}  className={`${st.main__container__button} ${st.main__container__button__signin}`}>
+                                <button className="btn">
+                                    SIGN IN WITH DISCORD
+                                </button>
+                            </a>
+                        }
+                        {auth?.token && 
+                            <a href="" className={`${st.main__container__button} ${st.main__container__button__signin}`}>
+                                <button className="btn">
+                                    JOIN THE DISCORD
+                                </button>
+                            </a>
+                        }
+                </div>
+
                 </div>
             </div>
             
