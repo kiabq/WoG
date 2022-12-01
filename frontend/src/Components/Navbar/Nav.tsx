211dc0bcdbd975c55667e06bb1622ce27011b4c2
kiabq <conway.joseph@pm.me>
2022-12-01 18:56:28 +0000

    Update dropdown to properly close on click outside of container
    Before, the nav dropdown had it's own div that would be sized to 100% of
    the screen height and width. This would be an issue, particularly in
    chrome (as far as I tested) because it would add a second scrollbar that
    would interfere with the normal window scrollbar. With the new solution,
    the user can close the nav dropdown when clicking outside of the
    dropdown component; the user can now also scroll the entire window without issue.

--- frontend/src/Components/Navbar/Nav.tsx
+++ frontend/src/Components/Navbar/Nav.tsx
@@ -1,55 +1,69 @@
 // Libraries
-import React, { useState, useEffect, useRef, FunctionComponent } from "react";
+import { useRef, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 // Styles
-import globals from '../../globals.module.css';
-import styles from './Nav.module.css';
+import styles from "./Nav.module.css";
 
 // Hooks
 import { useAuth } from "../../hooks/useProvider";
 import { useAcct } from "../../hooks/useAccount";
+import { useMobile } from "../../hooks/useMobile";
+
+function useOutsideCheck(ref: any) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        function handleModalChange(e: MouseEvent) {
+            if (ref.current && !ref.current.contains(e.target)) {
+                e.preventDefault();
+                e.stopPropagation();
+                setIsOpen(false);
+            }
+        }
+
+        document.addEventListener("click", handleModalChange)
+
+        return () => { 
+            document.removeEventListener("click", handleModalChange)
+        }
+    }, [ref])
+
+    return { isOpen, setIsOpen };
+}
 
 const Authed = () => {
-    const [style, setStyle] = useState<boolean>(false);
+    const wrapperRef = useRef(null);
+    const { accountInfo } = useAcct();
     const auth = useAuth();
-    const accountInfo = useAcct();
+    const isNavOpen = useOutsideCheck(wrapperRef);
 
     function logout() {
-        auth.logout();
-    }
-
-    function closeModal() {
-        setStyle(false);
+        auth?.logout();
     }
 
     return (
         <>
-            { style && <div className={styles.modal} onClick={() => closeModal()}/>}
-            
-            <div className={`${styles.nav__hamburger}`}>
-
-                <div className={`${styles.nav__account}`} onClick={() => {setStyle(!style)}}>
+            <div className={`${styles.nav__hamburger}`} ref={wrapperRef}>
+                <div className={`${styles.nav__account}`} onClick={() => isNavOpen.setIsOpen(!isNavOpen.isOpen)}>
                     <div className={styles.nav__account_info}>
                         <img src={accountInfo.avatar} className={styles.nav__account_pfp}/>
                         <p>{accountInfo.user}</p>
                     </div>
-                    <button className={`${globals.btn} ${styles.nav__dropdown_button}`}>
-                        <p className={`${styles.nav__dropdown_button_inner} ${style && styles.animate}`}>{'>'}</p>
+                    <button className={`${styles.nav__dropdown_button}`}>
+                        <p className={`${styles.nav__dropdown_button_inner} ${isNavOpen.isOpen && styles.animate}`}>{'>'}</p>
                     </button>
-                </div>
-
-                <div className={`${styles.nav__dropdown} ${styles.nav_test} ${style && styles.nav__dropdown_visible}`}>
-                    <div>
-                        <Link to='/account'>
-                            <button className={`${globals.btn} ${styles.nav__dropdown_element}`}>
-                                Account
-                            </button>
-                        </Link>
-                        <button className={`${globals.btn} ${styles.nav__dropdown_element}`} onClick={() => {logout()}}>
-                            Logout
+                </div>  
+                
+                <div className={`${styles.nav__dropdown} ${isNavOpen.isOpen && styles.nav__dropdown_visible}`}>
+                    <Link to='/account' className={styles.nav__dropdown__element}>
+                        <button className={`${styles.nav__dropdown_element}`}>
+                            Account
                         </button>
-                    </div>
+                    </Link>
+                    <button className={`${styles.nav__dropdown_element}`} onClick={() => logout()} >
+                        Logout
+                    </button>
                 </div>
             </div>
         </>
@@ -60,24 +74,28 @@ const NotAuthed = () => {
     return (
         <div className={styles.nav__buttons}>
             <a href={`${process.env.REACT_APP_BACKEND_URL}/api/connect/discord`}>
-                <button className={`${globals.btn} ${styles.nav__controls__btn}`}>Login with Discord</button>
+                <button className={``}>Sign-in with Discord</button>
             </a>
         </div>
     ) 
 }
 
 const Nav = () => {
-    let auth = useAuth();
-    
+    const auth = useAuth();
+    const isMobile = useMobile();
+
     return (
-        <header className={styles.nav}>
-            <div className={styles.nav__container}>
+        <header className={`${styles.nav}`}>
+            <div className={`${styles.nav__container} pd4`}>
                 <div className={styles.nav__info}>
-                    <p>Logo</p>
+                    <Link to='/' className="rb_bld_h4">
+                        WoG
+                    </Link>
                 </div>
-                <div className={styles.nav__controls}>
-                    {(auth.token && auth.user) && <Authed/>}
-                    {(!auth.token && !auth.user) && <NotAuthed />}
+
+                <div className={styles.nav__controls}>                    
+                    {(auth?.token && auth?.user) && <Authed/>}
+                    {(!auth?.token && !auth?.user) && <NotAuthed/>}
                 </div>
             </div>
         </header>
