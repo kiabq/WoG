3094a43abe7867ce67d6dc6200d6b4ccdab0066e
kiabq <conway.joseph@pm.me>
2022-12-01 18:43:56 +0000

    Update footer for proper flex positioning

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-
-import styles from './Footer.module.css';
+// Styles
+import st from "./Footer.module.css";
 
 const Footer = () => {
     return (
-        <footer className={styles.footer}>
-            <p>Contact</p>
-            <p>Twitch</p>
-            <p>&copy; Filler 2022</p>
+        <footer className={st.footer}>
+            <div className={`${st.footer__content} pd6-top`}>
+                <div className={st.footer__info}>
+                    <section>
+                        <h2 className={`${st.footer__info__header}`}>Socials</h2>
+                        <p>Discord</p>
+                        <p>Twitch</p>
+                    </section>
+                    <section>
+                        <h2 className={`${st.footer__info__header}`}>Contact</h2>
+                        <p>Email me!</p>
+                    </section>
+                </div>
+                <p>&copy; Filler 2022</p>
+            </div>
         </footer>
     )
 }
