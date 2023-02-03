c88598410f5637044fb5900d4b04b5635a502c2c
kiabq <conway.joseph@pm.me>
2023-02-03 18:09:04 -0500

    Update backend URL

--- backend/config/server.js
+++ backend/config/server.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
   // port: env.int('PORT', 1337),
   host: env('APP_HOST', '0.0.0.0'),
   port: env.int('NODE_PORT', 1337),
-  url: env('', 'https://starfish-app-cxov4.ondigitalocean.app/'),
+  url: env('', 'https://starfish-app-cxov4.ondigitalocean.app'),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET")
