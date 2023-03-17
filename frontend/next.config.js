56553167e936bebebaf38846377a4e850a0f9f12
kiabq <conway.joseph@pm.me>
2023-03-17 04:55:47 -0400

    update: tailwind and nextjs configs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
 }
 
 module.exports = nextConfig
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '4/5': '80%'
+      }
+    },
   },
   plugins: [],
 }
