1afbd5f7473740b2605a296d4094e54d49612bb7
kiabq <conway.joseph@pm.me>
2023-03-17 04:50:47 -0400

    feat: add layout for footer component

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <footer className='w-screen h-96 bg-slate-600'></footer>
+    )
+}
--- frontend/src/components/footer/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className='w-screen h-96 bg-slate-600'>
-
-        </footer>
-    )
-}
