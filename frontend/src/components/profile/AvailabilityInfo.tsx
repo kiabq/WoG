17a27a4a2a514c7f80467a6d95f35ba427d1c568
kiabq <conway.joseph@pm.me>
2023-03-22 23:17:33 -0400

    feat: add availability info component

---
+++ frontend/src/components/profile/AvailabilityInfo.tsx
@@ -0,0 +1,13 @@
+export default function AvailabilityInfo() {
+    return (
+        <div className='flex flex-row w-full h-48 text-center'>
+            <div className='grow basis-0'>Sunday</div>
+            <div className='grow basis-0'>Monday</div>
+            <div className='grow basis-0'>Tuesday</div>
+            <div className='grow basis-0'>Wednesday</div>
+            <div className='grow basis-0'>Thursday</div>
+            <div className='grow basis-0'>Friday</div>
+            <div className='grow basis-0'>Saturday</div>
+        </div>
+    )
+}
