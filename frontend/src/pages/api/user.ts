9ffcc99ed144b31a818f38a9d6267d6331cff4c7
kiabq <conway.joseph@pm.me>
2023-03-22 23:18:26 -0400

    feat: add api for handling user forms

---
+++ frontend/src/pages/api/user.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import Cookies from "cookies";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const cookies = new Cookies(req, res);
+    const token = cookies.get('token')
+    const body = req.body;
+    const config = {
+        Authorization: `Bearer ${token}`
+    }
+    const category = Object.keys(body)[0];
+
+    console.log(body);
+
+    await axios.put('http://localhost:1337/api/users/me/update',
+        body,
+        { headers: config }
+    ).then((response) => {
+        if (response.status !== 200) {
+            return res.status(response.status).json("Something went wrong.");
+        }
+
+        switch (category) {
+            case 'optionalQuestions':
+                return res.status(200).json(response.data.optionalQuestions);
+            case 'user_info':
+                return res.status(200).json(response.data.user_info);
+            default:
+                res.status(400);
+        }
+
+    }).catch((e) => {
+        // console.log("Failed: ", e);
+        res.status(400);
+    })
+}
