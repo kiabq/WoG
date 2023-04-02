7e5703825c4b8add035338c8a962d3a26d53b820
kiabq <conway.joseph@pm.me>
2023-04-01 21:13:30 -0400

    update: create form and api call when user submits availability info

--- frontend/src/components/profile/AvailabilityInfo.tsx
+++ frontend/src/components/profile/AvailabilityInfo.tsx
@@ -1,10 +1,13 @@
-import { FormEvent } from "react";
+// Libraries
+import { FormEvent } from 'react';
+import axios from 'axios';
 
 // Utils
-import { convertTime } from "@/utils/convertTime";
+import { convertTime } from '@/utils/convertTime';
 
 // Types
-import { Edit } from "@/utils/types";
+import { Edit } from '@/utils/types';
+import { IndexType } from '@/utils/types';
 
 export default function AvailabilityInfo({ user, edit, setEdit }: any) {
     const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
@@ -13,40 +16,65 @@ export default function AvailabilityInfo({ user, edit, setEdit }: any) {
         e.preventDefault();
 
         const form = {
-            sunday: user.sunday,
-            monday: user.monday,
-            tuesday: user.tuesday,
-            wednesday: user.wednesday,
-            thursday: user.thursday,
-            friday: user.friday,
-            saturday: user.saturday
+            sunday: user.sunday ?? { start_time: null, end_time: null },
+            monday: user.monday ?? { start_time: null, end_time: null },
+            tuesday: user.tuesday ?? { start_time: null, end_time: null },
+            wednesday: user.wednesday ?? { start_time: null, end_time: null },
+            thursday: user.thursday ?? { start_time: null, end_time: null },
+            friday: user.friday ?? { start_time: null, end_time: null },
+            saturday: user.saturday ?? { start_time: null, end_time: null }
         };
+        const submitted = e.currentTarget;
 
-        for (let i = 0; i < e.currentTarget.length; i++) {
-            if ((e.currentTarget[i] as HTMLFormElement).type === 'time') {
-                const day = (e.currentTarget[i] as HTMLFormElement).dataset.day;
-                const type = (e.currentTarget[i] as HTMLFormElement).dataset.type;
+        for (let i = 0; i < submitted.length; i++) {
+            if ((submitted[i] as HTMLFormElement).type === 'time') {
+                const day = (submitted[i] as HTMLFormElement).dataset.day;
+                const type = (submitted[i] as HTMLFormElement).dataset.type;
 
-                // console.log(day, type);
-                if (e.currentTarget[i].value) {
-                    form[day][type] = `${e.currentTarget[i].value}:00.000`;
+                if (submitted[i].value) {
+                    form[day][type] = `${submitted[i].value}:00.000`;
                 }
             }
+        }
+
+        if (form) {
+            await axios.put('http://localhost:3000/api/user', form).then((res) => {
+                if (res.status === 200) {}
+            });
         }
 
         setEdit(Edit.none);
     }
 
-
     return (
-        <form className='flex flex-col h-[23rem] relative md:h-[8rem]' onSubmit={(e) => submit(e)}>
+        <form className='flex flex-col h-[34rem] relative md:h-[8rem]' onSubmit={(e) => submit(e)}>
             <div className='flex flex-col w-full h-24 text-center md:flex-row'>
                 {days.map((day) => {
                     return (
-                        <div className='grow basis-0' key={day}>
+                        <div className='flex flex-col items-center grow basis-0' key={day}>
                             <p>{day[0].toUpperCase() + day.slice(1)}</p>
-                            <input type='time' data-day={day} data-type={'start_time'} defaultValue={user[day] && convertTime(user[day].start_time, true)} required></input>
-                            <input type='time' data-day={day} data-type={'end_time'} defaultValue={user[day] && convertTime(user[day].end_time, true)} required></input>
+                            <div className='w-fit'>
+                                <div className='flex text-left'>
+                                    <p className='pr-3 w-2/6'>From:</p>
+                                    <input className='w-4/6'
+                                        type='time'
+                                        data-day={day}
+                                        data-type='start_time'
+                                        defaultValue={user[day] && convertTime(user[day].start_time, true)}
+                                        required>
+                                    </input>
+                                </div>
+                                <div className='flex text-left'>
+                                    <p className='pr-3 w-2/6'>To:</p>
+                                    <input className='w-4/6'
+                                        type='time'
+                                        data-day={day}
+                                        data-type='end_time'
+                                        defaultValue={user[day] && convertTime(user[day].end_time, true)}
+                                        required>
+                                    </input>
+                                </div>
+                            </div>
                         </div>
                     )
                 })}
