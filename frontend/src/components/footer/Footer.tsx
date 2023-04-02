f11a1a035f9368f9ea077d2a121ede36d1f58e9d
kiabq <conway.joseph@pm.me>
2023-04-01 21:12:02 -0400

    update: add content + styling to footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,5 +1,24 @@
+// Libraries
+import Link from "next/link"
+
 export default function Footer() {
     return (
-        <footer className='w-screen h-96 bg-slate-600'></footer>
+        <footer className='flex flex-col w-screen h-96 pt-12 bg-slate-600 text-gray-100 items-center md:text-gray-400'>
+            <div className='flex pb-6'>
+                <section className='flex flex-col w-36 px-6'>
+                    <h2 className='pb-2 text-gray-100'>Socials</h2>
+                    <a href='' className='hover:text-gray-100'>Discord</a>
+                    <a href='' className='hover:text-gray-100'>Twitter</a>
+                    <a href='https://www.twitch.com' className='hover:text-gray-100'>Twitch</a>
+                </section>
+                <section className='px-6'>
+                    <h2 className='pb-2 text-gray-100'>Other</h2>
+                    <Link href='/privacy-policy' className='hover:text-gray-100'>
+                        Privacy Policy
+                    </Link>
+                </section>
+            </div>
+            <p className='text-gray-100'>&#169; 2023 - World of Gaian</p>
+        </footer>
     )
 }
