4e716598cc0ccc19f8264afa626dd430f05c25d6
kiabq <conway.joseph@pm.me>
2023-04-01 21:12:49 -0400

    feat: add mobile nav menu and event listener

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 // Libraries
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 
 // Fonts
 import localFont from 'next/font/local';
@@ -8,7 +9,7 @@ import { useEffect } from 'react';
 const rubik = localFont({ src: '../../fonts/rubik-variable.ttf' })
 
 type HeaderProps = {
-  user: any
+  user: any,
 }
 
 type LoaderArgs = {
@@ -16,23 +17,39 @@ type LoaderArgs = {
 }
 
 export default function Header(props: HeaderProps) {
+  const [isOpened, setIsOpened] = useState(false);
   const { user } = props;
 
   const loader = ({ src }: LoaderArgs) => {
     return `https://cdn.discordapp.com/avatars/${user.providerId}/${src}`
   }
 
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth >= 768) {
+        setIsOpened(false);
+      }
+    }
+    
+    window.addEventListener('resize', handleResize);
+
+    return (
+      window.addEventListener('resize', handleResize)
+    );
+  }, [])
+
   return (
     <header className={`${rubik.className} max-w-screen-lg mx-auto text-lg pb-3 border-b-2`}>
       <nav className='flex items-center pt-4'>
         <span className='grow'>
-          <Link href='/'>World of Gaian</Link>
+          <Link href='/' className='ml-3 md:ml-0'>World of Gaian</Link>
         </span>
-        <Link href='/#about' className='pr-3'>About</Link>
-        <Link href='/#dungeon-masters' className='pr-3'>Dungeon Masters</Link>
-        {!user.username && <Link href={`http://${process.env.REACT_APP_BACKEND}/api/connect/discord`}>Sign In With Discord</Link>}
-        {user.username &&
-          <div className=' border-2 rounded-lg'>
+
+        <Link href='/#about' className='pr-3 hidden md:block'>About</Link>
+        <Link href='/#dungeon-masters' className='pr-3 hidden md:block'>Dungeon Masters</Link>
+        {!user.username && <Link href={`http://${process.env.REACT_APP_BACKEND}/api/connect/discord`} className='hidden md:block'>Sign In With Discord</Link>}
+        {user &&
+          <div className='border-2 rounded-lg mr-3 hidden md:block hover:border-[#738adb]'>
             <Link href='/profile' className=' flex flex-row items-center'>
               <Image
                 loader={loader}
@@ -46,7 +63,40 @@ export default function Header(props: HeaderProps) {
             </Link>
           </div>
         }
+        {user && <Link href='/auth/logout' className='hidden md:block'>Sign Out</Link>}
+        <div>
+          <button className='relative group md:hidden' onClick={() => setIsOpened(!isOpened)}>
+            <div className='relative flex overflow-hidden items-center justify-center w-[50px] h-[50px] transform ring-0 duration-200'>
+              <div className='flex flex-col justify-between w-[20px] h-[20px] transform duration-300 origin-center overflow-hidden'>
+                {!isOpened && <>
+                  <div className='bg-black h-[2px] w-7 transform duration-300 origin-left delay-100'></div>
+                  <div className='bg-black h-[2px] w-7 rounded transform duration-300 delay-75'></div>
+                  <div className='bg-black h-[2px] w-7 transform duration-300 origin-left'></div>
+                </>}
+
+                {isOpened &&
+                  <div className='absolute items-center justify-between transform duration-500 top-2.5 -translate-x-10 translate-x-0 flex w-0 w-12'>
+                    <div className='absolute bg-black h-[2px] w-5 transform duration-500 rotate-0 delay-300 rotate-45'></div>
+                    <div className='absolute bg-black h-[2px] w-5 transform duration-500 -rotate-0 delay-300 -rotate-45'></div>
+                  </div>
+                }
+              </div>
+            </div>
+          </button>
+        </div>
       </nav>
+      {isOpened &&
+        <div className='flex flex-col text-left'>
+          <div className='mx-auto flex flex-col'>
+            {!user && <Link href={`http://${process.env.REACT_APP_BACKEND}/api/connect/discord`}>Sign In With Discord</Link>}
+            {user && <Link href='/profile'>Profile</Link>}
+            <Link href='/#about' className='pr-3'>About</Link>
+            <Link href='/#dungeon-masters' className='pr-3'>Dungeon Masters</Link>
+            {user && <Link href='/auth/logout' className='pr-3'>Sign Out</Link>}
+          </div>
+
+        </div>
+      }
     </header>
   )
 }
