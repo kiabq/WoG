839022b0dfb7f5c763e9cc4a7e77e0a45f6d3608
kiabq <conway.joseph@pm.me>
2023-06-09 22:50:52 -0400

    cleanup: remove unnecessary console logs

--- frontend/src/components/UI/timepicker.tsx
+++ frontend/src/components/UI/timepicker.tsx
@@ -14,7 +14,6 @@ export default function Timepicker({ classes, required, name, value }: IProps) {
             type='time' 
             required={required}
             name={name}
-            onChange={(e) => {console.log(e)}}
             defaultValue={value}
             className={`border-2 border-neutral-500 color-neutral-500 p-2 rounded-lg h-3/4 
                 ${classes ? classes : ''}`
