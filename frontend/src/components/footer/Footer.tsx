846c4745257acf87cbf44543559069339bab6de3
kiabq <conway.joseph@pm.me>
2023-06-13 20:17:25 -0400

    update: footer CSS
    - removed bg color and added a border

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -3,22 +3,22 @@ import Link from "next/link"
 
 export default function Footer() {
     return (
-        <footer className='flex flex-col w-screen h-96 pt-12 bg-slate-600 text-gray-100 items-center md:text-gray-400'>
+        <footer className='max-w-screen-lg mx-auto flex flex-col h-96 pt-12 text-black items-center border-t-2 md:text-gray-400'>
             <div className='flex pb-6'>
                 <section className='flex flex-col w-36 px-6'>
-                    <h2 className='pb-2 text-gray-100'>Socials</h2>
-                    <a href='' className='hover:text-gray-100'>Discord</a>
-                    <a href='' className='hover:text-gray-100'>Twitter</a>
-                    <a href='https://www.twitch.com' className='hover:text-gray-100'>Twitch</a>
+                    <h2 className='pb-2 text-black'>Socials</h2>
+                    <a href='' className='hover:text-black'>Discord</a>
+                    <a href='' className='hover:text-black'>Twitter</a>
+                    <a href='https://www.twitch.com' className='hover:text-black'>Twitch</a>
                 </section>
                 <section className='px-6'>
-                    <h2 className='pb-2 text-gray-100'>Other</h2>
-                    <Link href='/privacy-policy' className='hover:text-gray-100'>
+                    <h2 className='pb-2 text-black'>Other</h2>
+                    <Link href='/privacy-policy' className='hover:text-black'>
                         Privacy Policy
                     </Link>
                 </section>
             </div>
-            <p className='text-gray-100'>&#169; 2023 - World of Gaian</p>
+            <p className='text-black'>&#169; 2023 - World of Gaian</p>
         </footer>
     )
 }
