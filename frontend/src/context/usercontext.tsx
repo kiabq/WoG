cdb5eef52b93e6be06b971c9d3b2620f274cf24d
kiabq <conway.joseph@pm.me>
2023-06-15 07:50:32 -0400

    update: add state and state setter to update context

--- frontend/src/context/usercontext.tsx
+++ frontend/src/context/usercontext.tsx
@@ -1,17 +1,20 @@
 // Libraries
 import { IUser } from "@/utils/types";
-import React, { Context, ReactElement, createContext, useContext } from "react";
+import React, { Context, ReactElement, createContext, useContext, useState } from "react";
 
-const UserContext = createContext<IUser>({} as IUser);
+const UserContext = createContext<IProps>({} as IProps);
 
 interface IProps {
     user: IUser,
-    children: ReactElement[]
+    setAccount?: React.Dispatch<React.SetStateAction<IUser>>,
+    children?: ReactElement[]
 }
 
-export default function UserCtx({user, children}: IProps) {
+export default function UserCtx({ user: account, children }: IProps) {
+    const [user, setAccount] = useState(account);
+
     return (
-        <UserContext.Provider value={user}>
+        <UserContext.Provider value={{user, setAccount}}>
             {children}
         </UserContext.Provider>
     )
