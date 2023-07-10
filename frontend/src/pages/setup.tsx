426c5e19a0e3ffcd6c50171deb30e60292200946
kiabq <conway.joseph@pm.me>
2023-07-10 10:03:44 -0400

    feat: page to separate setup from profile component

---
+++ frontend/src/pages/setup.tsx
@@ -0,0 +1,68 @@
+// Libraries
+import Head from 'next/head';
+import { GetServerSideProps } from "next";
+import Cookies from 'cookies';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getUser } from '@/lib/getData';
+
+// Components
+import Header from '@/components/header/Header';
+import Setup from '@/components/profile/Setup';
+import Footer from '@/components/footer/Footer';
+
+// Context
+import UserCtx from '@/context/usercontext';
+
+// Types
+import type { IUser } from '@/utils/types';
+
+interface IProps {
+  user: IUser
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const cookies = new Cookies(req, res);
+  const token = cookies.get('token');
+  const user = await getUser(token);
+
+  return {
+    props: { user }
+  }
+}
+
+export default function Profile(props: IProps) {
+  const router = useRouter();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!props.user.isNew) {
+      router.push('/profile');
+    }
+
+    if (props.user === null) {
+      router.push('/auth/logout');
+    }
+
+    setLoaded(true);
+  }, [])
+
+  if (props.user && loaded === true) {
+    return (
+      <>
+        <Head>
+          <title>World of Gaian - Profile</title>
+        </Head>
+        <UserCtx user={props.user}>
+          <Header />
+          <main className='max-w-screen-lg px-6 mx-auto my-16'>
+            <Setup />
+          </main>
+          <Footer />
+        </UserCtx>
+      </>
+    )
+  } else {
+    return <div></div>
+  }
+}
