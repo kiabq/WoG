6112666cf8a44b4cc58a32bf0dff88b8d2b11ec3
kiabq <conway.joseph@pm.me>
2023-08-01 23:29:37 -0400

    feat: API route for getting all users

---
+++ frontend/src/pages/api/users.ts
@@ -0,0 +1,16 @@
+// Libraries
+import { getAllUsers } from '@/lib/getData';
+import axios from 'axios';
+import Cookies from 'cookies';
+
+// Types
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const cookies = new Cookies(req, res);
+    const token = cookies.get('token');
+    const filter = req.query.filter as string;
+    const response: Promise<any> = await getAllUsers(token).then((res) => { return res.data });
+    
+    return res.status(200).json(response);
+}
