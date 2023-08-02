2cbe3307a59d58b16e3fa6d6efb765fb9873a54f
kiabq <conway.joseph@pm.me>
2023-08-01 23:31:34 -0400

    update: admin panel search filter to a working state

--- frontend/src/pages/[admin-panel]/[page]/[pageSize].tsx
+++ frontend/src/pages/[admin-panel]/[page]/[pageSize].tsx
@@ -1,15 +1,19 @@
 // Libraries
 import Cookies from 'cookies';
 import Link from 'next/link';
+import axios from 'axios';
 import { GetServerSideProps } from "next";
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { getUser, getAllUsers } from '@/lib/getData';
 
 // Components
 import UserQuestions from '@/components/UI/UserQuestions';
 import DiscordAvatar from '@/components/UI/DiscordAvatar';
+import ChipFilter from '@/components/UI/ChipFilter';
 import { PageWrapper } from '@/components/UI/PageWrapper';
+
+// Utils
+import { getUser, getAllUsers } from '@/lib/getData';
 
 // Types
 import type { IUser } from '@/utils/types';
@@ -27,7 +31,7 @@ interface IProps {
 // TODO: Implement pagination with styled chips for filtering and styled page selectors
 export const getServerSideProps: GetServerSideProps = async ({ req, res, query }) => {
   const page = query.page ? parseInt(query['page'] as string) : 1;
-  const pageSize = query.pageSize ? parseInt(query['pageSize'] as string) : 5; 
+  const pageSize = query.pageSize ? parseInt(query['pageSize'] as string) : 5;
 
   const cookies = new Cookies(req, res);
   const token = cookies.get('token');
@@ -44,6 +48,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res, query }
   }
 }
 
+
 export default function UserPanel({ user, users: allUsers, pagination }: IProps) {
   const router = useRouter();
   const [users, setUsers] = useState(allUsers.data);
@@ -53,8 +58,6 @@ export default function UserPanel({ user, users: allUsers, pagination }: IProps)
   const [pageCount, setPageCount] = useState(pagination.pageCount);
   const [pageSize] = useState(pagination.pageSize);
 
-  console.log(router);
-
   useEffect(() => {
     if (user === null) {
       router.push('/auth/logout');
@@ -63,7 +66,43 @@ export default function UserPanel({ user, users: allUsers, pagination }: IProps)
 
   useEffect(() => {
     setUsers(allUsers.data);
-  }, [allUsers])
+    setPage(pagination.page);
+    setPageCount(pagination.pageCount);
+  }, [allUsers, pagination])
+
+  useEffect(() => {
+    handleSearch();
+  }, [search])
+
+  // TODO: When user clicks chip, filter all users by selected chips.
+  function chipReducer(state: any, action: any) {
+    switch (action.type) {
+      // ? Case Filter
+      // ? Case Remove
+      // ? Case Add New Filter - Maybe in the future.
+    }
+  }
+
+  async function handleSearch() {
+    const response = await axios.get(`${process.env.REACT_APP_FRONTEND}/api/users`)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        return err;
+      })
+
+    if (search === "") {
+      const left = (page * pageSize) - pageSize;
+      const right = page * pageSize;
+      setUsers(response.slice(left, right));
+    } else {
+      const filteredUsers = response.filter(
+        (user: IUser) => user.username.toLowerCase().includes(search)
+      );
+      setUsers(filteredUsers);
+    }
+  }
 
   function handleOpen(index: number) {
     if (opened !== index) {
@@ -73,8 +112,6 @@ export default function UserPanel({ user, users: allUsers, pagination }: IProps)
     }
   }
 
-  console.log(page, pageSize, pageCount);
-
   return (
     <PageWrapper user={user} title="World of Gaian - Admin Panel">
       <div>
@@ -83,74 +120,81 @@ export default function UserPanel({ user, users: allUsers, pagination }: IProps)
             className='border-2 border-slate-400 rounded-md w-full mb-2 p-2'
             placeholder='Search Users'
             onChange={(e) => {
-              setSearch(e.target.value.trim());
+              setSearch(e.currentTarget.value);
 
               if (opened) {
                 setOpened(null);
               }
             }}
           />
-          <button>New User</button>
-          <button>Admin</button>
-          <button>Authenticated User</button>
+          {/* <div className='mt-2 mb-5'>
+            <ChipFilter value={'New User'} />
+            <ChipFilter value={'Admin'} />
+            <ChipFilter value={'Authenticated'} />
+          </div> */}
         </div>
-        <div>
-          {users
-            .filter((user: IUser) => (user.username).toLowerCase().includes(search))
-            .map((user: IUser, index: number) => {
-              return (
-                <div className='flex flex-col my-5 border-2 border-slate-400 rounded-lg hover:border-slate-700 hover:cursor-pointer' onClick={() => { handleOpen(index) }}>
-                  <div className='flex'>
-                    <DiscordAvatar
-                      src={`${user.avatar}.png`}
-                      width={64}
-                      height={64}
-                      classes={`mr-3 ${opened === index ? 'rounded-tl-md' : 'rounded-l-md'}`}
-                      id={user.providerId}
-                      key={user.providerId}
-                    />
-                    <div className='flex flex-col justify-center'>
-                      <div className='flex items-center'>
-                        <p className='text-lg pr-3'>{user.username}</p>
-                        <p className='text-sm text-slate-600'>{user.role?.name}</p>
+        <div className='h-120 overflow-y-auto overflow-x-hidden'>
+          {users.length > 0 ?
+            users
+              .map((user: IUser, index: number) => {
+                return (
+                  <div className='flex flex-col my-5 border-2 border-slate-400 rounded-lg overflow-x-hidden hover:border-slate-700 hover:cursor-pointer' onClick={() => { handleOpen(index) }}>
+                    <div className='flex'>
+                      <DiscordAvatar
+                        src={`${user.avatar}.png`}
+                        width={64}
+                        height={64}
+                        classes={`mr-3 ${opened === index ? 'rounded-tl-md' : 'rounded-l-md'}`}
+                        id={user.providerId}
+                        key={user.providerId}
+                      />
+                      <div className='flex flex-col justify-center h-16'>
+                        <div className='flex items-center'>
+                          <p className='text-lg pr-3'>{user.username}</p>
+                          <p className='text-sm text-slate-600'>{user.role?.name}</p>
+                        </div>
+                        <p className='text-sm text-slate-600'>{user.availability?.timezone}</p>
                       </div>
-                      <p className='text-sm text-slate-600'>{user.availability?.timezone}</p>
+
+                    </div>
+                    <div className={`${opened === index ? 'block' : 'hidden'}`}>
+                      <UserQuestions optional={user.optionalQuestions} readonly={true} />
                     </div>
                   </div>
-                  <div className={`${opened === index ? 'block' : 'hidden'}`}>
-                    <UserQuestions optional={user.optionalQuestions} readonly={true} />
-                  </div>
-                </div>
-              )
-            })}
-        </div>
-        <div className='flex justify-center'>
-          <p className='w-9'>
-          {page > 1 &&
-            <Link 
-              href={`/admin-panel/${page - 1}/${pageSize}`} 
-              className='mx-1' 
-              onClick={() => { setPage(page - 1) }}>
-                Prev
-            </Link>
+                )
+              }) :
+            <p className='text-center my-10 md:my-20'>No Users Were Found</p>
           }
-          </p>
-          <div>
-            {Array.from({ length: pageCount }, (_, index) => (
-              <Link href={`/admin-panel/${index + 1}/${pageSize}`} className='mx-1' onClick={() => { setPage(index + 1) }}>{index + 1}</Link>
-            ))}
+        </div>
+        {search === "" &&
+          <div className='flex justify-center'>
+            <p className='w-9'>
+              {page > 1 &&
+                <Link
+                  href={`/admin-panel/${page - 1}/${pageSize}`}
+                  className=''
+                  onClick={() => { setPage(page - 1) }}>
+                  Prev
+                </Link>
+              }
+            </p>
+            <div>
+              {Array.from({ length: pageCount }, (_, index) => (
+                <Link href={`/admin-panel/${index + 1}/${pageSize}`} className={`${page === index + 1 && 'border-b-2'} border-slate-400 hover:border-black mx-1`} onClick={() => { setPage(index + 1) }}>{index + 1}</Link>
+              ))}
+            </div>
+            <p className='w-9'>
+              {page < pageCount &&
+                <Link
+                  href={`/admin-panel/${page + 1}/${pageSize}`}
+                  className=''
+                  onClick={() => { setPage(page + 1) }}>
+                  Next
+                </Link>
+              }
+            </p>
           </div>
-          <p className='w-9'>
-          {page < pageCount &&
-            <Link 
-              href={`/admin-panel/${page + 1}/${pageSize}`} 
-              className='mx-1' 
-              onClick={() => { setPage(page + 1) }}>
-                Next
-            </Link>
-          }
-          </p>
-        </div>
+        }
       </div>
     </PageWrapper>
   )
